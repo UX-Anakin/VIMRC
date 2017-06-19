892b1a527ce61ed148227274b224c475544f76f1
ChyiYaqing <chyiyaqing@gmail.com>
2017-06-20 00:14:43 +0800

    edit install_awesome_vimrc.sh add python.template

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -7,6 +7,7 @@ source ~/.vim_runtime/vimrcs/filetypes.vim
 source ~/.vim_runtime/vimrcs/plugins_config.vim
 source ~/.vim_runtime/vimrcs/extended.vim
 
+au BufNewFile *.py 0r ~/.vim_runtime/vimrcs/python.template
 try
 source ~/.vim_runtime/my_configs.vim
 catch
